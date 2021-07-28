ad1474e279666ed61e26c85d855dc2dc4e64f52b
GCostill <gaby123le@gmail.com>
2021-07-27 21:52:15 -0400

    challenges 1-3 complete

---
+++ JSONChallenge/PackageOrder.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONChallenge
+{
+    class PackageOrder
+    {
+        public string OrderId { get; set; }
+        public class Customer {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string PhoneNumber { get; set; }
+            public class ShippingAddress
+            {
+                public string AddressLine1 { get; set; }
+                public string AddressLine2 { get; set; }
+                public string City { get; set; }
+                public string State { get; set; }
+                public int Zip { get; set; }
+            }
+        }
+        //public List<string> LineItems
+        //{ }
+        //public string Sku { get; set; }
+        //public int Quantity { get; set; }
+        //public decimal UnitPrice { get; set; }
+        //public decimal TotalPrice { get; set; }
+        //public LineItems() { }
+        //public LineItems(
+        //    string sku,
+        //    int quantity,
+        //    decimal unitPrice,
+        //    decimal totalPrice
+        //    )
+        //{
+        //    Sku = sku;
+        //    Quantity = quantity;
+        //    UnitPrice = unitPrice;
+        //    TotalPrice = totalPrice;
+        //}
+    }
+}
---
+++ JSONChallenge/Program.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.FirstName = "Gaby";
+            person.LastName = "Costill";
+            person.DOB = new DateTime(2000, 01, 17);
+            person.FavoriteIceCreamFlavors = new List<string> { "strawberry", "cookie dough" };
+
+            string json = JsonConvert.SerializeObject(person);
+
+            Console.WriteLine(json);
+
+            Console.ReadKey();
+        }
+    }
+}
